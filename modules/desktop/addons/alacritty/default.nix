dfd597c56b3fb4b8aa4609f5dda8f86b3828e4ef
Alejandro Angulo <iam@alejandr0angul0.dev>
2023-03-29 22:11:30 -0700

    Tweaked opacity and font size

--- modules/desktop/addons/alacritty/default.nix
+++ modules/desktop/addons/alacritty/default.nix
@@ -23,9 +23,9 @@ in {
         programs.alacritty = {
           enable = true;
           settings = {
-            window.opacity = 0.9;
+            window.opacity = 0.95;
             font = {
-              size = 11.0;
+              size = 12.0;
               normal = {
                 family = "Hack Nerd Font";
                 style = "Regular";
