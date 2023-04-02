6b086db976c7b60d8499724ae8db1bf9c3e1dab4
Alejandro Angulo <iam@alejandr0angul0.dev>
2023-04-02 15:41:25 -0700

    Unified tmux, p10k, and nvim statuslines/prompts

--- modules/apps/neovim/default.nix
+++ modules/apps/neovim/default.nix
@@ -60,8 +60,8 @@ in {
               options = {
                 icons_enabled = true,
                 theme = 'auto',
-                component_separators = { left = '', right = ''},
-                section_separators = { left = '', right = ''},
+                component_separators = { left = '', right = ''},
+                section_separators = { left = '', right = ''},
                 disabled_filetypes = {
                 statusline = {},
                 winbar = {},
