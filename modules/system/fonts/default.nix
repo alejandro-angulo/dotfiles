f61238737f5a2f10b4af1963e3b995d5d945b678
Alejandro Angulo <iam@alejandr0angul0.dev>
2023-06-04 22:15:19 -0700

    Added fonts and updated lockfile

--- modules/system/fonts/default.nix
+++ modules/system/fonts/default.nix
@@ -14,6 +14,9 @@ in {
 
   config = mkIf cfg.enable {
     fonts.fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
       (nerdfonts.override {fonts = ["Hack"];})
     ];
   };
