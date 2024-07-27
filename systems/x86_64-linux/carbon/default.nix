77d376a3dcb5ae5e2cef3477341709876635d88b
Alejandro Angulo <iam@alejandr0angul0.dev>
2024-07-26 23:57:43 -0700

    Fixed issue with rebuilds failing

--- systems/x86_64-linux/carbon/default.nix
+++ systems/x86_64-linux/carbon/default.nix
@@ -35,6 +35,10 @@
     networkmanager.enable = true; # Enables wireless support via wpa_supplicant.
   };
 
+  # This service is problematic
+  # See: https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online.enable = false;
+
   time.timeZone = "America/Los_Angeles";
 
   # List packages installed in system profile. To search, run:
