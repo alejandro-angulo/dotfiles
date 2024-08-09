6eef2a92da956b59a1cc364007e7133c576568a4
alejandro-angulo <iam@alejandr0angul0.dev>
2024-08-08 17:20:45 -0700

    Added lazygit to git module
    This also integrates lazygit with delta

--- modules/home/tools/git/default.nix
+++ modules/home/tools/git/default.nix
@@ -83,5 +83,15 @@ in {
         };
       };
     };
+
+    programs.lazygit = {
+      enable = true;
+      settings = {
+        git.paging = {
+          colorArg = "always";
+          pager = "${pkgs.delta}/bin/delta --dark --paging=never";
+        };
+      };
+    };
   };
 }
