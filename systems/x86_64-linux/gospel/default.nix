c382929d9aa75f181f2e3596eb3bcf2400da59a5
alejandro-angulo <iam@alejandr0angul0.dev>
2024-10-06 20:37:09 -0700

    Fixed udev rules

--- systems/x86_64-linux/gospel/default.nix
+++ systems/x86_64-linux/gospel/default.nix
@@ -63,12 +63,18 @@
     system.monitoring.enable = true;
   };
 
-  services.udev.extraRules = ''
-    # CMSIS-DAP for microbit
-    ACTION!="add|change", GOTO="microbit_rules_end"
-    SUBSYSTEM=="usb", ATTR{idVendor}=="0d28", ATTR{idProduct}=="0204", TAG+="uaccess"
-    LABEL="microbit_rules_end"
-  '';
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "microbit-udev-rules";
+      text = ''
+        # CMSIS-DAP for microbit
+        ACTION!="add|change", GOTO="microbit_rules_end"
+        SUBSYSTEM=="usb", ATTR{idVendor}=="0d28", ATTR{idProduct}=="0204", TAG+="uaccess"
+        LABEL="microbit_rules_end"
+      '';
+      destination = "/etc/udev/rules.d/69-microbit.rules";
+    })
+  ];
 
   services.gitea-actions-runner.instances = {
     gospel = {
