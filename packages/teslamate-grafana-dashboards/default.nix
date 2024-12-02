997d1b2f9e5ab07c9b2109c1907d3d3925f066a2
alejandro-angulo <iam@alejandr0angul0.dev>
2024-12-01 17:02:55 -0800

    Updated teslamate grafana dashboards

--- packages/teslamate-grafana-dashboards/default.nix
+++ packages/teslamate-grafana-dashboards/default.nix
@@ -5,13 +5,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "teslamate-grafana-dashboards";
-  version = "1.31.0";
+  version = "1.32.0";
 
   src = fetchFromGitHub {
     owner = "teslamate-org";
     repo = "teslamate";
     rev = "v${version}";
-    hash = "sha256-aX6FjOKyjsA/0IgLw/AnB01ddNk0yS6vd01BH/reBP4=";
+    hash = "sha256-diQRtJYfzGIVLxrdBad3XKWCtR97rj9Q1ZJ9MmvJGRk=";
   };
 
   dontBuild = true;
