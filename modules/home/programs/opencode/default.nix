f13db2dba84e5ddf236ad45f2ba2b624a16c8982
alejandro-angulo <iam@alejandr0angul0.dev>
2025-07-13 21:45:29 -0700

    Configure context7 remote MCP

--- modules/home/programs/opencode/default.nix
+++ modules/home/programs/opencode/default.nix
@@ -15,7 +15,16 @@ in
   config = lib.mkIf cfg.enable {
     programs.opencode = {
       enable = true;
-      settings.theme = "catppuccin";
+      settings = {
+        theme = "catppuccin";
+        mcp = {
+          context7 = {
+            type = "remote";
+            url = "https://mcp.context7.com/mcp";
+            enabled = true;
+          };
+        };
+      };
     };
   };
 }
