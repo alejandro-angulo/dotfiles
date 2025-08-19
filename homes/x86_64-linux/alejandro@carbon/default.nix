0cc5c347d139de62dfc186491b1edfba7ae51239
alejandro-angulo <iam@alejandr0angul0.dev>
2025-08-18 19:33:06 -0700

    Disable sway

--- homes/x86_64-linux/alejandro@carbon/default.nix
+++ homes/x86_64-linux/alejandro@carbon/default.nix
@@ -4,5 +4,5 @@
   aa.windowManagers.sway.clamshell.enable = true;
   aa.programs.opencode.enable = true;
   aa.windowManagers.hyprland.enable = true;
-  aa.windowManagers.sway.enable = lib.mkForce true;
+  aa.windowManagers.sway.enable = lib.mkForce false;
 }
