8dc7858736f2e42b5a36c6eeeb3d4c76f3512f67
alejandro-angulo <iam@alejandr0angul0.dev>
2025-08-30 22:29:30 -0700

    Update monitor scaling

--- homes/x86_64-linux/alejandro@gospel/default.nix
+++ homes/x86_64-linux/alejandro@gospel/default.nix
@@ -6,6 +6,9 @@
     enable = true;
     settings.global.bitrate = 320;
   };
-  aa.windowManagers.hyprland.enable = true;
+  aa.windowManagers.hyprland = {
+    enable = true;
+    monitor = [ "HDMI-A-1,preferred,auto,1.25" ];
+  };
   aa.windowManagers.sway.enable = lib.mkForce false;
 }
