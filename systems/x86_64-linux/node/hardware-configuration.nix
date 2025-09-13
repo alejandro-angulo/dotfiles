1d8cd8c236bc5de5925e0a8e2077eff1eff61aeb
alejandro-angulo <iam@alejandr0angul0.dev>
2025-09-13 12:42:05 -0700

    Load amdgpu early

--- systems/x86_64-linux/node/hardware-configuration.nix
+++ systems/x86_64-linux/node/hardware-configuration.nix
@@ -21,7 +21,7 @@
     "usbhid"
     "sd_mod"
   ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
