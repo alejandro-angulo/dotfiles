6059bbb7325f4e895f77339a9c51d2e81fd24dbd
alejandro-angulo <iam@alejandr0angul0.dev>
2025-09-24 18:41:20 -0700

    Add wrapper for signal
    This fixes some issues I saw running with wayland

--- systems/x86_64-linux/carbon/default.nix
+++ systems/x86_64-linux/carbon/default.nix
@@ -63,7 +63,13 @@
     hugo
     nixos-generators
     vlc
-    signal-desktop-bin
+    (signal-desktop-bin.overrideAttrs (oldAttrs: {
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+      postInstall = oldAttrs.postInstall or "" + ''
+        wrapProgram $out/bin/signal-desktop \
+          --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
+      '';
+    }))
   ];
 
   programs.light.enable = true;
