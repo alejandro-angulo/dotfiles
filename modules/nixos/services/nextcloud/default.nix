c030afa0c51ba29019f5583ff5044b308a99f3ec
alejandro-angulo <iam@alejandr0angul0.dev>
2026-04-23 11:27:13 -0700

    Update nextcloud

--- modules/nixos/services/nextcloud/default.nix
+++ modules/nixos/services/nextcloud/default.nix
@@ -54,7 +54,7 @@ in
 
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud32;
+      package = pkgs.nextcloud33;
       hostName = "nextcloud.kilonull.com";
       https = true;
       database.createLocally = true;
