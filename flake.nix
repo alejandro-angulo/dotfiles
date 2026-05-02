394e8b9a0ab6656928cdaef310728a6b71311214
alejandro-angulo <iam@alejandr0angul0.dev>
2026-05-02 14:55:11 -0700

    Migrate away from snowfall to flake-parts

--- flake.nix
+++ flake.nix
@@ -5,6 +5,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -12,9 +15,6 @@
 
     nixos-generators.url = "github:nix-community/nixos-generators";
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
-
-    snowfall-lib.url = "github:snowfallorg/lib";
-    snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
 
     deploy-rs.url = "github:serokell/deploy-rs";
     deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
@@ -43,106 +43,327 @@
   };
 
   outputs =
-    inputs:
-    inputs.snowfall-lib.mkFlake {
-      inherit inputs;
-      src = ./.;
+    inputs@{ flake-parts, ... }:
+    let
+      lib = inputs.nixpkgs.lib;
+      namespace = "aa";
+      username = "alejandro";
+      homeDirectory = "/home/${username}";
 
-      # overlay-package-namespace = "aa";
-      snowfall.namespace = "aa";
+      nixpkgsConfig = {
+        android_sdk.accept_license = true;
+        allowUnfree = true;
+      };
 
-      channels-config.android_sdk.accept_license = true;
-      channels-config.allowUnfree = true;
+      packageDefinitions = pkgs: {
+        catppuccin-swaync = pkgs.callPackage ./packages/catppuccin-swaync { };
+        catppuccin-waybar = pkgs.callPackage ./packages/catppuccin-waybar { };
+        teslamate-grafana-dashboards = pkgs.callPackage ./packages/teslamate-grafana-dashboards { };
+      };
 
-      systems.modules.nixos = with inputs; [
-        agenix.nixosModules.default
-        home-manager.nixosModules.home-manager
-        catppuccin.nixosModules.catppuccin
+      packageOverlay = final: prev: {
+        ${namespace} = (prev.${namespace} or { }) // packageDefinitions final;
+      };
+
+      neovimOverlay = import ./overlays/neovim { inherit (inputs) nixvim; };
+      signalDesktopOverlay = import ./overlays/signal-desktop { };
+      defaultOverlay = lib.composeManyExtensions [
+        packageOverlay
+        neovimOverlay
+        signalDesktopOverlay
       ];
 
-      homes.modules = with inputs; [
-        catppuccin.homeModules.catppuccin
-        spicetify-nix.homeManagerModules.spicetify
-      ];
+      collectDefaultModules =
+        root:
+        let
+          collect =
+            prefix: dir:
+            let
+              entries = builtins.readDir dir;
+              thisModule =
+                if entries ? "default.nix" && entries."default.nix" == "regular" then
+                  {
+                    ${lib.concatStringsSep "/" prefix} = dir + "/default.nix";
+                  }
+                else
+                  { };
+              childModules = lib.mapAttrsToList (
+                name: type: if type == "directory" then collect (prefix ++ [ name ]) (dir + "/${name}") else { }
+              ) entries;
+            in
+            lib.foldl' lib.recursiveUpdate thisModule childModules;
+        in
+        collect [ ] root;
 
-      outputs-builder = channels: {
-        devShells.default = inputs.devenv.lib.mkShell {
-          inherit inputs;
-          pkgs = channels.nixpkgs;
-          modules = [ ./devenv.nix ];
+      localNixosModules = collectDefaultModules ./modules/nixos;
+      localHomeModules = collectDefaultModules ./modules/home;
+
+      specialArgs = {
+        inherit inputs namespace;
+      };
+
+      commonNixosModules =
+        builtins.attrValues localNixosModules
+        ++ (with inputs; [
+          agenix.nixosModules.default
+          home-manager.nixosModules.home-manager
+          catppuccin.nixosModules.catppuccin
+        ]);
+
+      commonHomeModules =
+        builtins.attrValues localHomeModules
+        ++ (with inputs; [
+          catppuccin.homeModules.catppuccin
+          spicetify-nix.homeManagerModules.spicetify
+        ]);
+
+      baseNixosModule = {
+        nixpkgs.config = nixpkgsConfig;
+        nixpkgs.overlays = [ defaultOverlay ];
+
+        home-manager.useGlobalPkgs = true;
+        home-manager.sharedModules = commonHomeModules;
+      };
+
+      mkHomeModule = homePath: {
+        ${namespace}.home.extraOptions = {
+          imports = [ homePath ];
+          home.username = username;
+          home.homeDirectory = homeDirectory;
         };
       };
 
-      deploy.nodes = {
-        node = {
-          hostname = "node";
-          profiles.system = {
-            user = "root";
-            sshUser = "alejandro";
-            path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos inputs.self.nixosConfigurations.node;
-            sshOpts = [ "-A" ];
+      mkNixosConfiguration =
+        {
+          system,
+          hostPath,
+          homePath,
+        }:
+        inputs.nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = specialArgs // {
+            inherit system;
           };
+          modules = commonNixosModules ++ [
+            baseNixosModule
+            {
+              home-manager.extraSpecialArgs = specialArgs // {
+                inherit system;
+              };
+            }
+            (mkHomeModule homePath)
+            hostPath
+          ];
         };
 
-        gospel = {
-          hostname = "gospel";
-          profiles.system = {
-            user = "root";
-            sshUser = "alejandro";
-            path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos inputs.self.nixosConfigurations.gospel;
-            sshOpts = [ "-A" ];
+      mkHomeConfiguration =
+        {
+          system,
+          homePath,
+        }:
+        inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            config = nixpkgsConfig;
+            overlays = [ defaultOverlay ];
           };
+          extraSpecialArgs = specialArgs // {
+            inherit system;
+          };
+          modules = commonHomeModules ++ [
+            homePath
+            {
+              home.username = username;
+              home.homeDirectory = homeDirectory;
+            }
+          ];
         };
 
-        git = {
-          hostname = "git.alejandr0angul0.dev";
-          profiles.system = {
-            user = "root";
-            sshUser = "alejandro";
-            path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos inputs.self.nixosConfigurations.git;
-            sshOpts = [ "-A" ];
-          };
-        };
+      helpers = import ./lib/helpers/default.nix { inherit lib; };
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { config, ... }:
+      {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
 
-        pi4 =
-          let
-            system = "aarch64-linux";
-            pkgs = import inputs.nixpkgs { inherit system; };
-            deployPkgs = import inputs.nixpkgs {
+        perSystem =
+          { system, pkgs, ... }:
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
               inherit system;
-              overlays = [
-                inputs.deploy-rs.overlays.default
-                (self: super: {
-                  deploy-rs = {
-                    inherit (pkgs) deploy-rs;
-                    lib = inputs.deploy-rs.lib;
-                  };
-                })
+              config = nixpkgsConfig;
+              overlays = [ defaultOverlay ];
+            };
+
+            packages = packageDefinitions pkgs;
+
+            devShells.default = inputs.devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                ./devenv.nix
               ];
             };
-          in
-          {
-            hostname = "pi4";
-            profiles.system = {
-              user = "root";
-              sshUser = "alejandro";
-              path = deployPkgs.deploy-rs.lib.aarch64-linux.activate.nixos inputs.self.nixosConfigurations.pi4;
-              # Usually deploy from an x86_64 machine and don't want to bother
-              # trying to build an aarch64 derivation
-              remoteBuild = true;
+          };
+
+        flake = {
+          lib = lib // helpers;
+
+          overlays = {
+            default = defaultOverlay;
+            neovim = neovimOverlay;
+            signal-desktop = signalDesktopOverlay;
+            "package/catppuccin-swaync" = final: prev: {
+              ${namespace} = (prev.${namespace} or { }) // {
+                catppuccin-swaync = (packageDefinitions final).catppuccin-swaync;
+              };
+            };
+            "package/catppuccin-waybar" = final: prev: {
+              ${namespace} = (prev.${namespace} or { }) // {
+                catppuccin-waybar = (packageDefinitions final).catppuccin-waybar;
+              };
+            };
+            "package/teslamate-grafana-dashboards" = final: prev: {
+              ${namespace} = (prev.${namespace} or { }) // {
+                teslamate-grafana-dashboards = (packageDefinitions final).teslamate-grafana-dashboards;
+              };
             };
           };
-      };
 
-      # nix eval .#domains --json
-      domains = inputs.self.lib.getDomainsPerHost [
-        "gospel"
-        "node"
-        "pi4"
-      ] inputs.self.nixosConfigurations;
+          nixosModules = localNixosModules;
+          homeModules = localHomeModules;
 
-      # TODO: Re-enable this when I figure out how to prevent needing to build
-      # dependencies for architectures other than the host machine
-      # checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks inputs.self.deploy) inputs.deploy-rs.lib;
-    };
+          nixosConfigurations = {
+            carbon = mkNixosConfiguration {
+              system = "x86_64-linux";
+              hostPath = ./systems/x86_64-linux/carbon;
+              homePath = ./homes/x86_64-linux + "/alejandro@carbon";
+            };
+            framework = mkNixosConfiguration {
+              system = "x86_64-linux";
+              hostPath = ./systems/x86_64-linux/framework;
+              homePath = ./homes/x86_64-linux + "/alejandro@framework";
+            };
+            git = mkNixosConfiguration {
+              system = "x86_64-linux";
+              hostPath = ./systems/x86_64-linux/git;
+              homePath = ./homes/x86_64-linux + "/alejandro@git";
+            };
+            gospel = mkNixosConfiguration {
+              system = "x86_64-linux";
+              hostPath = ./systems/x86_64-linux/gospel;
+              homePath = ./homes/x86_64-linux + "/alejandro@gospel";
+            };
+            node = mkNixosConfiguration {
+              system = "x86_64-linux";
+              hostPath = ./systems/x86_64-linux/node;
+              homePath = ./homes/x86_64-linux + "/alejandro@node";
+            };
+            pi4 = mkNixosConfiguration {
+              system = "aarch64-linux";
+              hostPath = ./systems/aarch64-linux/pi4;
+              homePath = ./homes/aarch64-linux + "/alejandro@pi4";
+            };
+          };
+
+          homeConfigurations = {
+            "alejandro@carbon" = mkHomeConfiguration {
+              system = "x86_64-linux";
+              homePath = ./homes/x86_64-linux + "/alejandro@carbon";
+            };
+            "alejandro@framework" = mkHomeConfiguration {
+              system = "x86_64-linux";
+              homePath = ./homes/x86_64-linux + "/alejandro@framework";
+            };
+            "alejandro@git" = mkHomeConfiguration {
+              system = "x86_64-linux";
+              homePath = ./homes/x86_64-linux + "/alejandro@git";
+            };
+            "alejandro@gospel" = mkHomeConfiguration {
+              system = "x86_64-linux";
+              homePath = ./homes/x86_64-linux + "/alejandro@gospel";
+            };
+            "alejandro@minimal" = mkHomeConfiguration {
+              system = "x86_64-linux";
+              homePath = ./homes/x86_64-linux + "/alejandro@minimal";
+            };
+            "alejandro@node" = mkHomeConfiguration {
+              system = "x86_64-linux";
+              homePath = ./homes/x86_64-linux + "/alejandro@node";
+            };
+            "alejandro@pi4" = mkHomeConfiguration {
+              system = "aarch64-linux";
+              homePath = ./homes/aarch64-linux + "/alejandro@pi4";
+            };
+          };
+
+          deploy.nodes = {
+            node = {
+              hostname = "node";
+              profiles.system = {
+                user = "root";
+                sshUser = "alejandro";
+                path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos config.flake.nixosConfigurations.node;
+                sshOpts = [ "-A" ];
+              };
+            };
+
+            gospel = {
+              hostname = "gospel";
+              profiles.system = {
+                user = "root";
+                sshUser = "alejandro";
+                path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos config.flake.nixosConfigurations.gospel;
+                sshOpts = [ "-A" ];
+              };
+            };
+
+            git = {
+              hostname = "git.alejandr0angul0.dev";
+              profiles.system = {
+                user = "root";
+                sshUser = "alejandro";
+                path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos config.flake.nixosConfigurations.git;
+                sshOpts = [ "-A" ];
+              };
+            };
+
+            pi4 =
+              let
+                system = "aarch64-linux";
+                pkgs = import inputs.nixpkgs { inherit system; };
+                deployPkgs = import inputs.nixpkgs {
+                  inherit system;
+                  overlays = [
+                    inputs.deploy-rs.overlays.default
+                    (self: super: {
+                      deploy-rs = {
+                        inherit (pkgs) deploy-rs;
+                        lib = inputs.deploy-rs.lib;
+                      };
+                    })
+                  ];
+                };
+              in
+              {
+                hostname = "pi4";
+                profiles.system = {
+                  user = "root";
+                  sshUser = "alejandro";
+                  path = deployPkgs.deploy-rs.lib.aarch64-linux.activate.nixos config.flake.nixosConfigurations.pi4;
+                  remoteBuild = true;
+                };
+              };
+          };
+
+          domains = helpers.getDomainsPerHost [
+            "gospel"
+            "node"
+            "pi4"
+          ] config.flake.nixosConfigurations;
+        };
+      }
+    );
 }
