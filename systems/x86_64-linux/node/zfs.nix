204fa600a9135b4cb2d715a3ae3d3a8557e25bd3
alejandro-angulo <iam@alejandr0angul0.dev>
2026-05-16 17:28:46 -0700

    Disable forceImportRoot
    Fixes a warning

--- systems/x86_64-linux/node/zfs.nix
+++ systems/x86_64-linux/node/zfs.nix
@@ -1,10 +1,9 @@
 {
-  config,
-  pkgs,
   ...
 }:
 {
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.extraPools = [ "tank" ];
+  boot.zfs.forceImportRoot = false;
   networking.hostId = "db616c9e";
 }
