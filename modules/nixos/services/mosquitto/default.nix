3d510e78b8f4d97cb47da4e01f5ce16538f85e2d
alejandro-angulo <iam@alejandr0angul0.dev>
2026-08-30 21:37:40 -0700

    fix: fix typo in mqtt acl

--- modules/nixos/services/mosquitto/default.nix
+++ modules/nixos/services/mosquitto/default.nix
@@ -28,7 +28,7 @@ in
                 "readwrite home/#"
                 "readwrite homeassistant/#"
                 "readwrite zigbee2mqtt/#"
-                "readwrite frigate/*"
+                "readwrite frigate/#"
                 "read teslamate/#"
               ];
               passwordFile = config.age.secrets.hass_mqtt.path;
